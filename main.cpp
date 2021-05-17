54f175159a76f61dabeb9d095b4055d636dc0945
AmirBazanov <flash5651311@gmail.com>
2021-05-18 01:06:25 +0500

    Downgrade windows resolution (for more speed), trying to draw 3D

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ public:
     Map(RenderWindow &wn){
 		window = &wn;
         wn_size =  Vector2i(wn.getSize().x, wn.getSize().y);
-        line_count = (wn_size.x+wn_size.y)/50+1;
+        line_count = (wn_size.x+wn_size.y)/25+1;
         grid = new RectangleShape[line_count];
 
     }
@@ -40,8 +40,8 @@ public:
         for(int row=0; row<15;row++){
             for(int collum=0;collum<29;collum++){
                 if(map[row][collum]){
-                    map_objects = RectangleShape({50,50});
-                    map_objects.setPosition((float)collum*50, (float)row*50);
+                    map_objects = RectangleShape({25,25});
+                    map_objects.setPosition((float)collum*25, (float)row*25);
                     switch (map[row][collum]) {
                         case 1: map_objects.setFillColor(Color(100,100,100)); break;
                         case 2: map_objects.setFillColor(Color::Blue); break;
@@ -55,15 +55,15 @@ public:
 
      void drawGrid(){
         for(int i =0; i<line_count; i++){
-            if(i>wn_size.x/50){
-                int y_pos = i-wn_size.x/50;
+            if(i>wn_size.x/25){
+                int y_pos = i-wn_size.x/25;
                 grid[i] = RectangleShape(Vector2f((float)wn_size.x, 1));
-                grid[i].setPosition(0, (float)y_pos*50);
+                grid[i].setPosition(0, (float)y_pos*25);
                 grid[i].setFillColor(Color(80, 80, 80));
             }
             else {
                 grid[i] = RectangleShape(Vector2f(1, (float)wn_size.y));
-                grid[i].setPosition((float)i*50, 0);
+                grid[i].setPosition((float)i*25, 0);
                 grid[i].setFillColor(Color(80, 80, 80));
             }
         }
@@ -79,16 +79,23 @@ private:
     RectangleShape player;
     Vector2f hit_position;
     Vertex line[2];
+    Vertex wall[2];
+    float horizon;
+    float max_wall_height;
     float fov;
     float distance;
-    RenderWindow *window;
+    RenderWindow *mp_wn;
+    RenderWindow *gm_wn;
     Vector2f position;
 
 public:
-    Player(RenderWindow &wn) {
+    Player(RenderWindow &wn_m, RenderWindow &wn_g) {
         position = Vector2f(100, 100);
         fov = 60;
-        window = &wn;
+		distance = 0;
+        mp_wn = &wn_m;
+        gm_wn = &wn_g;
+        horizon = mp_wn->getSize().y/2;
         player = RectangleShape({5, 5});
         player.setPosition(position);
         player.setFillColor(Color::Green);
@@ -113,44 +120,61 @@ public:
     }
 
     void findObstacle(int map[][29]) {
-        window->draw(player);
-        for (int i = 0; i < window->getSize().x; i++) {
-            float angle = player.getRotation() - fov / 2 + fov * i / window->getSize().x;
+        mp_wn->draw(player);
+        for (int i = 0; i < mp_wn->getSize().x; i++) {
+            Color color;
+            float angle = player.getRotation() - fov / 2 + fov * i / (mp_wn->getSize().x);
             distance = 0;
-            for (; distance < 500; distance += 1) {
+            for (; distance < 100; distance += 1) {
                 hit_position = {(float) (player.getPosition().x + distance * cos(angle * toRadians)),
                                 (float) (player.getPosition().y + distance * sin(angle * toRadians))};
-                if (map[(int) (hit_position.y / 50)][(int) (hit_position.x / 50)] != 0) break;
+                if (map[(int) (hit_position.y / 25)][(int) (hit_position.x / 25)] != 0){
+                    switch (map[(int) (hit_position.y / 25)][(int) (hit_position.x / 25)]) {
+                        case 1: color = Color(100,100,100); break;
+                        case 2: color = Color::Blue; break;
+                        case 3: color = Color::Red; break;
+                    }
+                    wall[0] = Vertex({(float)i,horizon}, color);
+                    wall[1] = Vertex({(float)i, horizon/sqrt(distance)}, color);
+                    gm_wn->draw(wall, 2, Lines);
+                    break;
+                }
             }
             line[0] = Vertex(player.getPosition());
             line[1] = Vertex(hit_position);
-            window->draw(line, 2, Lines);
+            mp_wn->draw(line, 2, Lines);
+
+
         }
     }
 };
 
+
 int main()
 {
-    RenderWindow window(VideoMode(1450, 750), "SFML Engine");
-    Player pl(window);
-    Map map(window);
+    RenderWindow map_window(VideoMode(725, 375), "Map");
+    RenderWindow game_window(VideoMode(725, 375), "3D");
+    Player pl(map_window, game_window);
+    Map map(map_window);
     Event event{};
-    while (window.isOpen())
+    while (map_window.isOpen())
     {
-        while (window.pollEvent(event))
+        while (map_window.pollEvent(event))
         {
             if (event.type == Event::Closed)
             {
-                window.close();
+                map_window.close();
             }
 
         }
-        window.clear();
+        map_window.clear();
+        game_window.clear();
         map.drawGrid();
         map.drawMap();
         pl.moving(0.5);
         pl.findObstacle(map.map);
-        window.display();
+        map_window.display();
+        game_window.display();
     }
 
 }
