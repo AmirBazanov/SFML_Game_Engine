12e0b049afe51fbb53e14ead122ea9c6eba65b4e
AmirBazanov <flash5651311@gmail.com>
2021-05-26 14:00:43 +0500

    3D drawing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <cstdio>
+#include <stdlib.h>
 #include <cmath>
 #define toRadians 3.14/180
 using namespace sf;
@@ -95,11 +95,24 @@ public:
 		distance = 0;
         mp_wn = &wn_m;
         gm_wn = &wn_g;
-        horizon = mp_wn->getSize().y/2;
+        horizon = (float)gm_wn->getSize().y-100;
         player = RectangleShape({5, 5});
         player.setPosition(position);
         player.setFillColor(Color::Green);
 
+    }
+
+    int alfa(int dis){
+        int alfa;
+        if(dis!=0) alfa = 6000/dis;
+        else{return 20;}
+        if(alfa>=255){
+            return 255;
+        }
+        if(alfa<=20){
+            return 20;
+        }
+        return alfa;
     }
 
     void moving(float speed) {
@@ -121,28 +134,35 @@ public:
 
     void findObstacle(int map[][29]) {
         mp_wn->draw(player);
-        for (int i = 0; i < mp_wn->getSize().x; i++) {
+        for (int i = 0; i < (int)mp_wn->getSize().x; i++) {
             Color color;
             float angle = player.getRotation() - fov / 2 + fov * i / (mp_wn->getSize().x);
             distance = 0;
-            for (; distance < 100; distance += 1) {
+            for (; distance < 300; distance += 1) {
                 hit_position = {(float) (player.getPosition().x + distance * cos(angle * toRadians)),
                                 (float) (player.getPosition().y + distance * sin(angle * toRadians))};
+
                 if (map[(int) (hit_position.y / 25)][(int) (hit_position.x / 25)] != 0){
                     switch (map[(int) (hit_position.y / 25)][(int) (hit_position.x / 25)]) {
-                        case 1: color = Color(100,100,100); break;
-                        case 2: color = Color::Blue; break;
-                        case 3: color = Color::Red; break;
+                        case 1: color = Color(100,100,100, alfa((int)distance)); break;
+                        case 2: color = Color(0,0,204, alfa((int)distance)); break;
+                        case 3: color = Color(204,0,0, alfa((int)distance)); break;
                     }
-                    wall[0] = Vertex({(float)i,horizon}, color);
-                    wall[1] = Vertex({(float)i, horizon/sqrt(distance)}, color);
-                    gm_wn->draw(wall, 2, Lines);
+
+                    RectangleShape rec = RectangleShape({1, gm_wn->getSize().y*25/(distance)});
+                    rec.setPosition({(float )i, horizon-(distance/5)});
+                    rec.setRotation(180);
+                    rec.setFillColor(color);
+                    gm_wn->draw(rec);
                     break;
                 }
+
             }
-            line[0] = Vertex(player.getPosition());
-            line[1] = Vertex(hit_position);
-            mp_wn->draw(line, 2, Lines);
+
+//			RectangleShape rec = RectangleShape({ distance,1 });
+//			rec.setRotation(player.getRotation());
+//			mp_wn->draw(rec);
+
 
 
         }
@@ -154,26 +174,31 @@ int main()
 {
     RenderWindow map_window(VideoMode(725, 375), "Map");
     RenderWindow game_window(VideoMode(725, 375), "3D");
+    game_window.setFramerateLimit(50);
     Player pl(map_window, game_window);
     Map map(map_window);
     Event event{};
     while (map_window.isOpen())
     {
-        while (map_window.pollEvent(event))
+        while (game_window.pollEvent(event))
         {
             if (event.type == Event::Closed)
             {
+                game_window.close();
                 map_window.close();
             }
 
         }
-        map_window.clear();
+//        map_window.clear();
         game_window.clear();
-        map.drawGrid();
-        map.drawMap();
+        RectangleShape rec = RectangleShape({(float)game_window.getSize().x, (float)game_window.getSize().y});
+        rec.setFillColor(Color(135, 206, 235));
+        game_window.draw(rec);
+//        map.drawGrid();
+//        map.drawMap();
         pl.moving(0.5);
         pl.findObstacle(map.map);
-        map_window.display();
+//        map_window.display();
         game_window.display();
     }
 
